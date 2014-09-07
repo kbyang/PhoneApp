df7094439b651226bc0168a02ffc1c03af5120b3
unknown <Sresht@sr-pc.(none)>
2014-09-06 23:03:28 -0400

    rudimentary sms prototype

--- PhoneApp/PhoneApp/Resources/Resource.designer.cs
+++ PhoneApp/PhoneApp/Resources/Resource.designer.cs
@@ -24,6 +24,10 @@ namespace PhoneApp
 			global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 		}
 		
+		public static void UpdateIdValues()
+		{
+		}
+		
 		public partial class Attribute
 		{
 			
@@ -44,10 +48,13 @@ namespace PhoneApp
 			public const int Icon = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int StaggerLogoFinal3 = 2130837505;
+			public const int monoandroidsplash = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int StaggerLogoFinal5 = 2130837506;
+			public const int StaggerLogoFinal3 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int StaggerLogoFinal5 = 2130837507;
 			
 			static Drawable()
 			{
@@ -62,41 +69,53 @@ namespace PhoneApp
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050006
-			public const int buttonBadLines = 2131034118;
+			// aapt resource value: 0x7f06000a
+			public const int SMSLayout = 2131099658;
 			
-			// aapt resource value: 0x7f050005
-			public const int buttonGoodLines = 2131034117;
+			// aapt resource value: 0x7f060006
+			public const int buttonBadLines = 2131099654;
 			
-			// aapt resource value: 0x7f050008
-			public const int buttonpress = 2131034120;
+			// aapt resource value: 0x7f060005
+			public const int buttonGoodLines = 2131099653;
 			
-			// aapt resource value: 0x7f05000a
-			public const int data = 2131034122;
+			// aapt resource value: 0x7f060008
+			public const int buttonpress = 2131099656;
 			
-			// aapt resource value: 0x7f05000b
-			public const int imageView1 = 2131034123;
+			// aapt resource value: 0x7f06000e
+			public const int data = 2131099662;
 			
-			// aapt resource value: 0x7f050001
-			public const int level = 2131034113;
+			// aapt resource value: 0x7f06000f
+			public const int imageView1 = 2131099663;
 			
-			// aapt resource value: 0x7f050000
-			public const int linearLayout1 = 2131034112;
+			// aapt resource value: 0x7f060001
+			public const int level = 2131099649;
 			
-			// aapt resource value: 0x7f050002
-			public const int myButton = 2131034114;
+			// aapt resource value: 0x7f060000
+			public const int linearLayout1 = 2131099648;
 			
-			// aapt resource value: 0x7f050007
-			public const int resultPickupLine = 2131034119;
+			// aapt resource value: 0x7f060002
+			public const int myButton = 2131099650;
 			
-			// aapt resource value: 0x7f050003
-			public const int resultTest = 2131034115;
+			// aapt resource value: 0x7f06000c
+			public const int phoneNumberForm = 2131099660;
 			
-			// aapt resource value: 0x7f050009
-			public const int resultbuttonclick = 2131034121;
+			// aapt resource value: 0x7f06000b
+			public const int phoneNumberText = 2131099659;
 			
-			// aapt resource value: 0x7f050004
-			public const int textView1 = 2131034116;
+			// aapt resource value: 0x7f060007
+			public const int resultPickupLine = 2131099655;
+			
+			// aapt resource value: 0x7f060003
+			public const int resultTest = 2131099651;
+			
+			// aapt resource value: 0x7f060009
+			public const int resultbuttonclick = 2131099657;
+			
+			// aapt resource value: 0x7f06000d
+			public const int submitButton = 2131099661;
+			
+			// aapt resource value: 0x7f060004
+			public const int textView1 = 2131099652;
 			
 			static Id()
 			{
@@ -157,6 +176,22 @@ namespace PhoneApp
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Mono_Android_Theme_Splash = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
--- PhoneApp/PhoneApp/SMSActivity.cs
+++ PhoneApp/PhoneApp/SMSActivity.cs
@@ -10,6 +10,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Telephony;
 
 namespace PhoneApp
 {
@@ -19,9 +20,26 @@ namespace PhoneApp
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.SMSLayout);
 
-			// Create your application here
+//			this.Window.SetSoftInputMode(
+//				WindowManager.Handle.);
+
+			Button submitButton = FindViewById<Button>(Resource.Id.submitButton);
+			submitButton.Click += delegate {
+				EditText e = FindViewById<EditText>(Resource.Id.phoneNumberForm);
+				string phoneNumber = e.Text;
+				if( phoneNumber == null || phoneNumber.Length != 10)
+				{
+//					Toast.MakeText(this, "phone number must be 10 digits!", ToastLength.Short);
+					Console.WriteLine("phone number not long");
+					e.Text = "";
+				}
+				else
+				{
+					SmsManager.Default.SendTextMessage("2488918624", null, "Hey, I inputted your number into Stagger the app because I love you! Will you have dirty sex with me?", null, null);
+				}
+			};
 		}
 	}
 }
-
